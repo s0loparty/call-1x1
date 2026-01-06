4c3238c29fc9c12c81bf09ace67bf2c946b37332
s0loparty <izovo@bk.ru>
2026-01-06 17:25:39 +0300

    fix: add server option for config/broadcasting reverb connection

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,6 +40,11 @@ return [
                 'port' => env('REVERB_PORT', 443),
                 'scheme' => env('REVERB_SCHEME', 'https'),
                 'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                'server' => [
+                    'host' => 'reverb',
+                    'port' => '8080',
+                    'scheme' => 'http',
+                ],
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
